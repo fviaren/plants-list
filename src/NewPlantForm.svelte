2e52b58e44b025507645896f61df6f9b88d93549
Alaa Sarhan <sarhan.alaa@protonmail.com>
2020-04-15 22:36:33 +0200

    add validation to new plant form

--- src/NewPlantForm.svelte
+++ src/NewPlantForm.svelte
@@ -1,23 +1,34 @@
 <script>
-    let name;
-    let wateringFrequency;
-    let wateringAmount;
+    export let name = '';
+    export let wateringFrequency = '';
+    export let wateringAmount = '';
     export let saveCallback;
     export let cancelCallback;
+    const digitsRegex = /^[0-9]+$/;
     function onSave() {
         saveCallback({name, wateringFrequency, wateringAmount});
     }
+
+    function updateValidData() {
+        const nameIsValid = name && name.length;
+        const frequencyIsValid = wateringFrequency && digitsRegex.test(wateringFrequency);
+        const amountIsValid = wateringAmount && digitsRegex.test(wateringAmount);
+
+        validData = nameIsValid && frequencyIsValid && amountIsValid;
+    }
+
+    let validData = false;
 </script>
 
 <form class="NewPlantForm">
     <h3>New Plant</h3>
     <div class="NewPlantForm__inputGroup">
-        <input type="text" bind:value={name} placeholder="Input the name of the plant" />
-        <input type="text" bind:value={wateringFrequency} placeholder="Input the watering frequency of your plant" />
-        <input type="text" bind:value={wateringAmount} placeholder="Input the watering amount for your plant" />
+        <input type="text" on:change={updateValidData} bind:value={name} placeholder="Input the name of the plant" />
+        <input type="text" on:change={updateValidData} bind:value={wateringFrequency} placeholder="Input the watering frequency of your plant" />
+        <input type="text" on:change={updateValidData} bind:value={wateringAmount} placeholder="Input the watering amount for your plant" />
     </div>
     
-    <button on:click={onSave}>Save Plant</button>
+    <button on:click={onSave} disabled={!validData}>Save Plant</button>
     <button on:click={cancelCallback}>Cancel</button>
 
 </form>
