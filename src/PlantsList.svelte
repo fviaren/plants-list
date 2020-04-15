ae125646a7b21df897cefad3e6092d98a9bc224a
Alaa Sarhan <sarhan.alaa@protonmail.com>
2020-04-15 20:40:08 +0200

    initial commit

---
+++ src/PlantsList.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Plant from './Plant.svelte'
+    export let plants;
+</script>
+
+<ul>
+{#each plants as plant}
+    <li>
+        <Plant plant={plant}></Plant>
+    </li>
+{/each}
+</ul>
+
+<style>
+</style>
---
+++ src/fakeItems.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        name: 'Tulip',
+        wateringFreq: 2,
+        wateringAmount: 'low',
+    },
+    {
+        name: 'Cactus',
+        wateringFreq: 10,
+        wateringAmount: 'very low',
+    },
+    {
+        name: 'Rose',
+        wateringFreq: 1,
+        wateringAmount: 'medium',
+    },
+]
