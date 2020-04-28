3c6148bbba13029c88d028958d5ba9b6b8cdd576
Alaa Sarhan <sarhan.alaa@protonmail.com>
2020-04-28 23:02:33 +0200

    add reminders on/off toggle

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,38 @@
 <script>
 	import PlantsList from './PlantsList.svelte'
 	
+	let remindersOn = Boolean(localStorage.getItem('remindersOn'));
 	let plants = JSON.parse(localStorage.getItem('plants')) || [];
-
+	
 	function onPlantsChanged(newPlantsList) {
 		plants = newPlantsList;
 		localStorage.setItem('plants', JSON.stringify(plants));
 	}
 
+	async function toggleReminders() {
+		if (remindersOn) {
+			remindersOn = false;
+		} else {
+			const permission = await Notification.requestPermission();
+			
+			if (permission === 'granted') {
+				remindersOn = true;
+			}
+		}
+
+		localStorage.setItem('remindersOn', remindersOn);
+	};
 </script>
 
 <main>
+	<button on:click={toggleReminders}>{remindersOn ? 'ON' : 'OFF'}</button>
 	<PlantsList plants={plants} plantsChangedCallback={onPlantsChanged}></PlantsList>
 </main>
 
 <style>
 	main {
 		display: flex;
+		flex-direction: column;
 		justify-content: center;
 		padding: 1em;
 		margin: 0 auto;
